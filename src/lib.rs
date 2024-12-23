a20722a89c9205eb423d2cc50dd2b860020ce4aa
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-12-23 10:56:56 -0500

    chore: extract library

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! Various utilities and types for interfacing with Figura's avatars. This crate currently
+//! includes:
+//!
+//! * [Loading avatars from a file][Moon::read]
+//! * [Running avatars in-memory][crate::runtime]
+//! * [Serving avatars to users][Backend::run]
+//! packing/unpacking/repacking moon files and editing assets.
+
+#![allow(warnings)]
+#![deny(missing_docs)]
+#![feature(never_type)]
+
+pub mod moon;
+pub use moon::Moon;
